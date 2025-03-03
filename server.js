470a7b45923ccf73e655feb5a97584d580eeebe3
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-03-03 20:11:43 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files (like your APP.HTML)
+// Serve static files (like APP.HTML)
 app.use(express.static(__dirname));
 
 // Serve APP.HTML as the main page
